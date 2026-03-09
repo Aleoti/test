fdc52673575fe7a21f2e2c7b77d4af3de4ee47c3
Alexey Ilin <alex.ilinf20@mail.ru>
2026-03-10 01:05:39 +0800

    yo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module test
 
 go 1.25.1
+
+require github.com/buger/jsonparser v1.1.1
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Pole struct {
+	Type string `json:"type"`
+}
+type Slov struct {
+	S []map[string]interface{}
+}
 
 func main() {
-	fmt.Println("Hello")
+	response, err := http.Get("https://api.github.com/users/Aleoti/events/public")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data []Pole
+	err = json.Unmarshal([]byte(body), &data)
+	//fmt.Println(data)
+	for _, v := range data {
+		fmt.Println(v.Type)
+	}
 }
